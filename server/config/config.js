6c8d0fc9fb8ad962dace77ae2ecef41904c5e0e4
Ragemouse <rasmus.krister.moberg@gmail.com>
2019-02-14 16:08:42 +0100

    Fixed some small issues

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@
   var env = process.env.NODE_ENV || "development";
   if(env === "development"){
     process.env.PORT = 3000;
-    process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
   }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;//Man skriver över mongoose egna Promise-model eftersom att promise
 //finns inbyggd.
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = {mongoose};
